70c1e88b331dae13f522aef0f914ce55837efcc9
cris <cristian.alvarez-b@mail.escuelaing.edu.co>
2024-08-22 09:38:03 -0500

    Primer parte Co-authored-by: juli <bricenojuliana@gmail.com>

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -1,14 +1,34 @@
 package edu.eci.arsw.primefinder;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.Scanner;
 public class Main {
 
-	public static void main(String[] args) {
-		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
-		
-		pft.start();
-		
-		
+	public static void main(String[] args) throws InterruptedException {
+		Scanner scanner = new Scanner(System.in);
+		AtomicBoolean flag = new AtomicBoolean(false);
+		PrimeFinderThread thread1=new PrimeFinderThread(0, 999999999, flag);
+		PrimeFinderThread thread2 = new PrimeFinderThread(1000000000, 1999999999, flag);
+		PrimeFinderThread thread3 = new PrimeFinderThread(2000000000, new Long("3000000000"), flag);
+		thread1.start();
+		thread2.start();
+		thread3.start();
+
+		while (thread1.isAlive() || thread2.isAlive() || thread3.isAlive()) {
+			// Pausa todos los hilos
+			Thread.sleep(5000);
+			flag.set(true);
+			scanner.nextLine();
+
+			// Reanuda todos los hilos
+			synchronized (flag) {
+				flag.set(false);
+				flag.notifyAll();
+			}
+		}
+
+		scanner.close();
 	}
-	
 }
+
+
--- parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -2,32 +2,45 @@ package edu.eci.arsw.primefinder;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class PrimeFinderThread extends Thread{
 
 	
-	int a,b;
+	long a,b;
+	AtomicBoolean flag;
+	private List<Long> primes=new LinkedList<Long>();
 	
-	private List<Integer> primes=new LinkedList<Integer>();
-	
-	public PrimeFinderThread(int a, int b) {
+	public PrimeFinderThread(long a, long b, AtomicBoolean flag) {
 		super();
 		this.a = a;
 		this.b = b;
+		this.flag = flag;
+
 	}
 
 	public void run(){
-		for (int i=a;i<=b;i++){						
+		try{
+			finder();
+		}catch (InterruptedException ex){
+			ex.printStackTrace();
+		}
+
+	}
+	private void finder() throws InterruptedException{
+		for (long i=a;i<=b;i++){
+			synchronized (flag){
+				while (flag.get()){
+					flag.wait();
+				}
+			}
 			if (isPrime(i)){
 				primes.add(i);
 				System.out.println(i);
 			}
 		}
-		
-		
 	}
-	
-	boolean isPrime(int n) {
+	boolean isPrime(long n) {
 	    if (n%2==0) return false;
 	    for(int i=3;i*i<=n;i+=2) {
 	        if(n%i==0)
@@ -36,7 +49,7 @@ public class PrimeFinderThread extends Thread{
 	    return true;
 	}
 
-	public List<Integer> getPrimes() {
+	public List<Long> getPrimes() {
 		return primes;
 	}
 	
